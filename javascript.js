b68ae69a343cf652526f91fe5aa6742f895fe9f0
GeraldFishta <ge.fishta@gmail.com>
2023-04-17 23:00:45 +0200

    basta

--- javascript.js
+++ javascript.js
@@ -10,16 +10,30 @@ async function getQuestions(){
 getQuestions();
 
 
-//array vuoti e ciclo push
-//timer
-//far apparire la domanda con le 4 risposte
-//domanda div create.element 1 
-//4 buttoni con create.element 2
-//variabile somma risposte
+let size= 100
+let timeLeft = 60
+function reduceBorder(){
+
+  
+  size = Math.abs(timeLeft - 0)
+  border.style.width = `${size}%`;
+  border.style.height = `${size}%`
+}
 
 
-//timer : 
+let timer = setInterval(() => {
+  timeLeft--;
+  document.getElementById('timer').textContent = timeLeft;
+  if (timeLeft === 0) {
+  clearInterval(timer);
+}
+}, 1000);
+ 
 
+
+
+
+/*
 let timeLeft = 0;
 for (let i = 0; i <questions.lenght; i++) {
     if (questions[i].difficulty == "medium") {
@@ -28,15 +42,4 @@ for (let i = 0; i <questions.lenght; i++) {
         timeLeft = 45
     }
 } 
-
-let timer = setInterval(() => {
-  timeLeft--;
-  document.getElementById('timer').textContent = timeLeft;
-  let degrees = (timeLeft / 60) * 360 - 90;
-  document.getElementById('timer').style.transform = `rotate(${degrees}deg)`;
-  let opacity = timeLeft / 60;
-  document.getElementById('timer').style.borderColor = `rgba(0, 0, 255, ${opacity})`;
-  if (timeLeft === 0) {
-    clearInterval(timer);
-  }
-}, 1000);
+*/
