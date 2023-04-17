ea4d4468d2ca24aa03e898e4f31c4fdc84e4d804
Luci02 <luciparlato50@gmail.com>
2023-04-17 14:36:04 +0200

    aggiunto file JS e funzione che prende le domande

---
+++ assets/js/style.js
@@ -0,0 +1,7 @@
+async function getQuestions(){
+    let domande = await fetch('https://opentdb.com/api.php?amount=10&category=18').then(res => res.json()).then(res => res.results);
+
+    console.log(domande);
+}
+
+getQuestions();
