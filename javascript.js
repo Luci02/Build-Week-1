49c4743bdfd3755d61acdf9218cd94d28056258d
GeraldFishta <ge.fishta@gmail.com>
2023-04-18 11:01:11 +0200

    Co-authored-by: Luci02 <Luci02@users.noreply.github.com>

--- javascript.js
+++ javascript.js
@@ -10,15 +10,96 @@ async function getQuestions(){
 getQuestions();
 
 
-let timeLeft = 60
-let timer = setInterval(() => {
-  timeLeft--;
-  document.getElementById('timer').textContent = timeLeft;
-  if (timeLeft === 0) {
-  clearInterval(timer);
+const FULL_DASH_ARRAY = 283;
+const TIME_LIMIT = 20;
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">
+  <p class="seconds">Seconds</p>
+  ${formatTime(timeLeft)}
+  <p class="remaining">Remaining</p>
+  </span>
+</div>
+`;
+
+startTimer();
+
+function onTimesUp() {
+  clearInterval(timerInterval);
 }
-}, 1000);
- 
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+    document.getElementById("base-timer-label").innerHTML = `<p class="seconds">Seconds</p> 
+    ${formatTime(timeLeft)}
+    <p class="remaining">Remaining</p>`;
+    setCircleDasharray();
+    
+
+    if (timeLeft === 0) {
+      onTimesUp();
+    }
+  }, 1000);
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  return `${seconds}`;
+}
+
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
@@ -30,4 +111,16 @@ for (let i = 0; i <questions.lenght; i++) {
         timeLeft = 45
     }
 } 
+*/
+/*
+function timer(){
+let timeLeft = 60
+let timer = setInterval(() => {
+  timeLeft--;
+  document.getElementById('timer').textContent = timeLeft;
+  if (timeLeft === 0) {
+  clearInterval(timer);
+}
+}, 1000); 
+}
 */
