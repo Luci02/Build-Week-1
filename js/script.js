a7b7dea91476ca2fa9afbb51f23735df15f29401
Richard-94 <rich.boat82@gmail.com>
2023-04-18 14:44:41 +0200

    aggiunti commenti

--- js/script.js
+++ js/script.js
@@ -2,41 +2,53 @@
 
 //CREAZIONE DELLE STELLE
 function createStars() {
-  let starsContainer = document.querySelector("#stars");
+  let contenitoreStelle = document.querySelector("#stars");
   for (let i = 0; i < 10; i++) {
     let star = document.createElement("span");
     star.classList.add("star");
-    starsContainer.appendChild(star);
+    contenitoreStelle.appendChild(star);
   }
 }
 
 //AGGIUNGERE HOVER SU TUTTE LE STELLE
-function addHoverEffect() {
+function aggiungiHover() {
  let stars = document.querySelectorAll("#stars .star");
-  stars.forEach((star) => {
-    star.addEventListener("mouseover", highlight);
-    star.addEventListener("mouseout", removeHighlight);
+  stars.forEach(function(questeStelle){
+    questeStelle.addEventListener("mouseover",illumina);
+    questeStelle.addEventListener("mouseout", togliIllumina);
   });
 }
+//La funzione aggiungiHover va a selezionare tutte le stelle create con la classe "star", le cicla e per ogni stella "forEach" attiva un event listener che quando 
+// ci passo il mouse sopra attiva la funzione "illumina", quando rimuovo il mouse attiva la funzione "togliIllumina"
+
+
 
 //ADD HIGHLIGHT CLASS ON HOVER
-function highlight() {
-  let stars = document.querySelectorAll("#stars .star");
-  let starIndex = Array.from(stars).indexOf(this);
+function illumina() {
+  let seleziona = document.querySelectorAll("#stars .star");
+  let starIndex = Array.from(seleziona).indexOf(this);
   for (let i = 0; i <= starIndex; i++) {
-    stars[i].classList.add("starHover");
+    seleziona[i].classList.add("starHover");
   }
 }
 
-//REMOVE HIGHLIGHT CLASS ON MOUSEOUT
-function removeHighlight() {
+// La funzione "illumina" = il variabile "starIndex"  (che a sua volta è stata selezionata da "seleziona"), attiva il metodo "Array.from" che sempre con il ciclo divide le stelle
+// una volta alla volta e con "indexOf(this)" cioè seleziona "QUESTO-THIS"  cioe l'elemento su cui si trova il mouse in quel preciso momento.
+// poi per ogni stella aggiunge la classe starHover definito in css prima.
+
+
+//TOGLIERE LA FUNZIONE ILLUMINA 
+function togliIllumina() {
   const stars = document.querySelectorAll("#stars .star");
-  stars.forEach((star) => {
-    star.classList.remove("starHover");
+  stars.forEach(function(star) {
+    star.classList.add("starHover");
   });
 }
 
-//FILL STARS UNTIL THE SELECTED STAR
+
+
+
+//RIEMPIRE LE STELLE FINCHE QUELLA DESIDERATA NON VIENE CLICCATA
 function fillStars(target) {
   let activeStarClass = "starActive";
   let inactiveStarClass = "starInactive";
@@ -53,8 +65,8 @@ function fillStars(target) {
 }
 
 //ADD CLICK EVENT LISTENER TO STARS CONTAINER
-let starsContainer = document.querySelector("#stars");
-starsContainer.addEventListener("click", function(event) {
+let contenitoreStelle = document.querySelector("#stars");
+contenitoreStelle.addEventListener("click", function(event) {
   if (event.target.matches(".star")) {
     fillStars(event.target);
   }
@@ -63,4 +75,4 @@ starsContainer.addEventListener("click", function(event) {
 
 //INITIALIZE THE STARS
 createStars();
-addHoverEffect();
+aggiungiHover();
