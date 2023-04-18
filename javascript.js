48287c89bc06af0e89d620236f88f3f54311f29e
GeraldFishta <ge.fishta@gmail.com>
2023-04-18 13:05:04 +0200

    prova1

--- javascript.js
+++ javascript.js
@@ -1,17 +1,80 @@
-
-async function getQuestions(){
-    let domande = await fetch('https://opentdb.com/api.php?amount=10&category=18').then(res => res.json()).then(res => res.results);
-
-    console.log(domande);
+let sum = 0;
 
 
+async function getQuestions() {
+    let questions = await fetch('https://opentdb.com/api.php?amount=10&category=18').then(res => res.json()).then(res => res.results);
+    
+    console.dir(questions);
+    
+    //ciclo le domande una per una
+    for (let question of questions) {
+        
+        //creo un array vuoto e pusho tutte le domande
+        let options = [];
+        options.push(question["correct_answer"]);
+        for (let element of question["incorrect_answers"]) {
+            options.push(element);
+        }
+
+        //salvo in una variabile la risposta corretta
+        let correctAnswer = question["correct_answer"];
+
+        //con la funzione shuffleArray() mescolo gli elementi contenuti nell'array
+        shuffleArray(options);
+
+        //stampo le domande
+        for (let risposta of options) {
+
+            //salvo la difficoltà in una variabile
+            let difficulty = risposta.difficulty;
+            let time = 0;
+
+            switch (true) {
+                case difficulty == 'easy':
+                    time = 16;
+                    break;
+                case difficulty == 'medium':
+                    time = 40;
+                    break;
+                case difficulty == 'hard':
+                    time = 60;
+                    break;
+            }
+
+            //seleziono l'elemento con l'id #domanda e gli cambio il contenuto
+            // let domanda = document.querySelector('#domanda');
+            // domanda.textContent = question.question;
+
+            // let bottone = document.createElement('button');
+            // bottone.textContent = risposta;
+
+            //AGGIUNGERE LE VARIE CLASSI AL BOTTONE
+            // let buttonContainer = document.querySelector('#button-container');
+            // bottone.classList.add('');
+            // buttonContainer.append(bottone);
+
+        }
+
+    }
 }
 
+//funzione per mescolare gli elementi di un array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+//eseguo la funzione getQuestions
 getQuestions();
 
+console.log(options)
+console.log(time)
 
 const FULL_DASH_ARRAY = 283;
-const TIME_LIMIT = 20;
+let TIME_LIMIT = time;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 let timerInterval = null;
@@ -100,27 +163,3 @@ function setCircleDasharray() {
 
 
 
-
-
-/*
-let timeLeft = 0;
-for (let i = 0; i <questions.lenght; i++) {
-    if (questions[i].difficulty == "medium") {
-        timeLeft = 60
-    }else{
-        timeLeft = 45
-    }
-} 
-*/
-/*
-function timer(){
-let timeLeft = 60
-let timer = setInterval(() => {
-  timeLeft--;
-  document.getElementById('timer').textContent = timeLeft;
-  if (timeLeft === 0) {
-  clearInterval(timer);
-}
-}, 1000); 
-}
-*/
