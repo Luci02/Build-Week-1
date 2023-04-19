bfe07fa79e91fc9f3d3514a030d5e3cbcf754fe8
GeraldFishta <ge.fishta@gmail.com>
2023-04-19 10:25:21 +0200

    prove

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,148 +1,306 @@
-let sum = 0;
+//seleziono il template che ho fatto in html
+let template = document.getElementsByTagName('template')[0];
+let contatore = 0;
+let risposte = {
+    giuste: 0,
+    sbagliate: 0,
+}
 
-let template = document.getElementsByTagName("template")[0];
 async function getQuestions() {
     let questions = await fetch('https://opentdb.com/api.php?amount=10&category=18').then(res => res.json()).then(res => res.results);
 
-
     console.dir(questions);
-    
-    
-    for( let i = 0; i < questions.length ; i++){
-        displayQuestion(i)
+
+    //clono il contenuto, generando ogni volta un nuovo clone
+    let clone = template.content.cloneNode(true);
+
+    //creo un array vuoto e pusho tutte le domande
+    let options = [];
+    options.push(questions[contatore]["correct_answer"]);
+    for (let element of questions[contatore]["incorrect_answers"]) {
+        options.push(element);
     }
 
-    //ciclo le domande una per una
-    
-    function displayQuestion(index) {
-        if (index >= questions.length) {
-            target.removeChild(target)
-        }
-    
-        let question = questions[index];
-        let clone = template.content.cloneNode(true);
+    //con la funzione shuffleArray() mescolo gli elementi contenuti nell'array
+    shuffleArray(options);
 
-        for (let question of questions) {
-        
+    let buttonContainer = clone.querySelector('#button-container');
+    console.log(options);
+
+    //stampo i bottoni con le risposte
+    for (let risposta of options) {
+        //seleziono l'elemento con l'id #domanda e gli cambio il contenuto
+        let domanda = clone.querySelector('#domanda');
+        domanda.textContent = questions[contatore].question;
+
+        let bottone = document.createElement('button');
+        bottone.textContent = risposta;
+
+        //AGGIUNGERE LE VARIE CLASSI AL BOTTONE
+        // bottone.classList.add('');
+        buttonContainer.append(bottone);
+    }
+
+    //aggiungo l'evento click ed aumento l'indice
+    for (let bottone of buttonContainer.children) {
+        bottone.addEventListener('click', function () {
+            if (this.textContent == questions[contatore]["correct_answer"]) {
+                risposte.giuste++;
+            } else {
+                risposte.sbagliate++;
+            }
+            if(contatore < questions.length){
+                document.getElementById('button-container').innerHTML = '';
+                aggiornaDomanda();
+            }
+        });
+    }
+
+    let tempo = 0;
+
+    switch (true) {
+        case questions[contatore].difficulty == 'easy':
+            tempo = 3;
+            break;
+        case questions[contatore].difficulty == 'medium':
+            tempo = 5;
+            break;
+        case questions[contatore].difficulty == 'hard':
+            tempo = 6;
+            break;
+        default:
+            tempo = 5;
+    }
+
+    const FULL_DASH_ARRAY = 283;
+    let TIME_LIMIT = tempo;
+    let timePassed = 0;
+    let timeLeft = TIME_LIMIT;
+    let timerInterval = null;
+
+
+    clone.getElementById("app").innerHTML = `
+            <div class="base-timer">
+                <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g class="base-timer__circle">
+                <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+                <path
+                id="base-timer-path-remaining"
+                stroke-dasharray="283"
+                class="base-timer__path-remaining"
+                d="
+                M 50, 50
+                m -45, 0
+                a 45,45 0 1,0 90,0
+                a 45,45 0 1,0 -90,0
+                "
+                ></path>
+                </g>
+                </svg>
+                <span id="base-timer-label" class="base-timer__label">
+                    <p class="seconds">Seconds</p>
+                    ${formatTime(timeLeft)}
+                    <p class="remaining">Remaining</p>
+                </span>
+            </div>
+            `;
+
+    //faccio partire il timer
+    startTimer();
+
+    //definisco un'area in cui inserire il clone
+    let target = document.getElementById("target");
+
+    //inserisco il clone
+    target.append(clone);
+
+    //funzioni relative al timer
+    function onTimesUp() {
+        clearInterval(timerInterval);
+        contatore++;
+        faiDomanda();
+    }
+
+    function startTimer() {
+        timerInterval = setInterval(() => {
+            timePassed = timePassed += 1;
+            timeLeft = TIME_LIMIT - timePassed;
+            document.getElementById("base-timer-label").innerHTML = `<p class="seconds">Seconds</p>
+            ${formatTime(timeLeft)}
+            <p class="remaining">Remaining</p>`;
+            setCircleDasharray();
+
+
+            if (timeLeft === 0) {
+                onTimesUp();
+                aggiornaDomanda();
+            }
+        }, 1000);
+    }
+
+    function formatTime(time) {
+        let seconds = time % 60;
+        return `${seconds}`;
+    }
+
+    function calculateTimeFraction() {
+        const rawTimeFraction = timeLeft / TIME_LIMIT;
+        return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+    }
+
+    function setCircleDasharray() {
+        const circleDasharray = `${(
+            calculateTimeFraction() * FULL_DASH_ARRAY
+        ).toFixed(0)} 283`;
+        document
+            .getElementById("base-timer-path-remaining")
+            .setAttribute("stroke-dasharray", circleDasharray);
+    }
+
+    function faiDomanda() {
+        //definisco un'area in cui inserire il clone
+        let target = document.getElementById("target");
+
+        while (target.hasChildNodes()) {
+            target.removeChild(target.firstChild);
+        }
+
+        let clone = template.content.cloneNode(true);
+        //inserisco il clone
+        target.append(clone);
+    }
+
+    function aggiornaDomanda() {
         //creo un array vuoto e pusho tutte le domande
         let options = [];
-        options.push(question["correct_answer"]);
-        for (let element of question["incorrect_answers"]) {
+        options.push(questions[contatore]["correct_answer"]);
+        for (let element of questions[contatore]["incorrect_answers"]) {
             options.push(element);
         }
 
-        //salvo in una variabile la risposta corretta
-        let correctAnswer = question["correct_answer"];
-        
         //con la funzione shuffleArray() mescolo gli elementi contenuti nell'array
         shuffleArray(options);
+
+        let buttonContainer = document.querySelector('#button-container');
+        console.log(options);
 
         //stampo i bottoni con le risposte
         for (let risposta of options) {
             //seleziono l'elemento con l'id #domanda e gli cambio il contenuto
-            let domanda = clone.querySelector('#domanda');
-            domanda.textContent = question.question;
+            let domanda = document.querySelector('#domanda');
+            domanda.textContent = questions[contatore].question;
 
             let bottone = document.createElement('button');
             bottone.textContent = risposta;
 
-            bottone.addEventListener('click', () => {
-                clearInterval(timerInterval);
-                target.removeChild(bottone);
-                target.appendChild(bottone);
-                displayQuestion(index + 1);
-            });
             //AGGIUNGERE LE VARIE CLASSI AL BOTTONE
-            let buttonContainer = clone.querySelector('#button-container');
             // bottone.classList.add('');
             buttonContainer.append(bottone);
-            
+        }
+
+        //aggiungo l'evento click ed aumento l'indice
+        for (let bottone of buttonContainer.children) {
+            bottone.addEventListener('click', function () {
+                if (this.textContent == questions[contatore]["correct_answer"]) {
+                    risposte.giuste++;
+                } else {
+                    risposte.sbagliate++;
+                }
+                let target = document.getElementById("button-container");
+                while (target.hasChildNodes()) {
+                    target.removeChild(target.firstChild);
+                }
+                if(contatore < questions.length){
+                    aggiornaDomanda();
+                }
+                contatore++;
+            });
         }
 
         let tempo = 0;
-        //salvo la difficoltà in una variabile
-        let difficulty = question.difficulty;
 
-            switch (true) {
-                case difficulty == 'easy':
-                    tempo = 16;
-                    break;
-                case difficulty == 'medium':
-                    tempo = 40;
-                    break;
-                case difficulty == 'hard':
-                    tempo = 60;
-                    break;
-                default:
-                    tempo = 5;
-            }
+        switch (true) {
+            case questions[contatore].difficulty == 'easy':
+                tempo = 5;
+                break;
+            case questions[contatore].difficulty == 'medium':
+                tempo = 4;
+                break;
+            case questions[contatore].difficulty == 'hard':
+                tempo = 3;
+                break;
+            default:
+                tempo = 5;
+        }
 
-        //faccio partire il timer
         const FULL_DASH_ARRAY = 283;
         let TIME_LIMIT = tempo;
         let timePassed = 0;
         let timeLeft = TIME_LIMIT;
         let timerInterval = null;
 
-        clone.getElementById("app").innerHTML = `
-        <div class="base-timer">
-          <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-            <g class="base-timer__circle">
-              <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-              <path
+        document.getElementById("app").innerHTML = `
+            <div class="base-timer">
+                <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                <g class="base-timer__circle">
+                <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+                <path
                 id="base-timer-path-remaining"
                 stroke-dasharray="283"
                 class="base-timer__path-remaining"
                 d="
-                  M 50, 50
-                  m -45, 0
-                  a 45,45 0 1,0 90,0
-                  a 45,45 0 1,0 -90,0
+                M 50, 50
+                m -45, 0
+                a 45,45 0 1,0 90,0
+                a 45,45 0 1,0 -90,0
                 "
-              ></path>
-            </g>
-          </svg>
-          <span id="base-timer-label" class="base-timer__label">
-          <p class="seconds">Seconds</p>
-          ${formatTime(timeLeft)}
-          <p class="remaining">Remaining</p>
-          </span>
-        </div>
-        `;
+                ></path>
+                </g>
+                </svg>
+                <span id="base-timer-label" class="base-timer__label">
+                    <p class="seconds">Seconds</p>
+                    ${formatTime(timeLeft)}
+                    <p class="remaining">Remaining</p>
+                </span>
+            </div>
+            `;
 
         startTimer();
 
+        //funzioni relative al timer
         function onTimesUp() {
             clearInterval(timerInterval);
+            contatore++;
+            faiDomanda();
         }
-        
+
         function startTimer() {
             timerInterval = setInterval(() => {
                 timePassed = timePassed += 1;
                 timeLeft = TIME_LIMIT - timePassed;
-                document.getElementById("base-timer-label").innerHTML = `<p class="seconds">Seconds</p> 
+                document.getElementById("base-timer-label").innerHTML = `<p class="seconds">Seconds</p>
             ${formatTime(timeLeft)}
             <p class="remaining">Remaining</p>`;
                 setCircleDasharray();
-        
-        
+
+
                 if (timeLeft === 0) {
                     onTimesUp();
+                    aggiornaDomanda();
                 }
             }, 1000);
         }
-        
+
         function formatTime(time) {
             let seconds = time % 60;
-        
             return `${seconds}`;
         }
-        
+
         function calculateTimeFraction() {
             const rawTimeFraction = timeLeft / TIME_LIMIT;
             return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
         }
-        
+
         function setCircleDasharray() {
             const circleDasharray = `${(
                 calculateTimeFraction() * FULL_DASH_ARRAY
@@ -152,12 +310,22 @@ async function getQuestions() {
                 .setAttribute("stroke-dasharray", circleDasharray);
         }
 
-
-        let target = document.getElementById("target")
-        target.appendChild(clone);
-        console.log(target)
+        function faiDomanda() {
+            //definisco un'area in cui inserire il clone
+            let target = document.getElementById("target");
+    
+            while (target.hasChildNodes()) {
+                target.removeChild(target.firstChild);
+            }
+    
+            let clone = template.content.cloneNode(true);
+            //inserisco il clone
+            target.append(clone);
+        }
     }
 }
+
+
 
 //funzione per mescolare gli elementi di un array
 function shuffleArray(array) {
@@ -168,8 +336,5 @@ function shuffleArray(array) {
     return array;
 }
 
-}
-
-
 //eseguo la funzione getQuestions
-getQuestions()
+getQuestions();
