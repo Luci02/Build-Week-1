36b9bbdd82f75b571823f0c8bbcef8df8d563ed5
GeraldFishta <ge.fishta@gmail.com>
2023-04-20 14:23:17 +0200

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,6 @@ let contatore = 0;
 let risposte = {
     giuste: 0,
     sbagliate: 0,
-    "risposte date": [],
 }
 
 async function getQuestions() {
@@ -49,7 +48,6 @@ async function getQuestions() {
         buttonContainer.append(bottone);
 
         bottone.addEventListener('click', function () {
-            risposte["risposte date"].push(this.textContent);
             if (this.textContent == questions[contatore]["correct_answer"]) {
                 risposte.giuste++;
             } else {
@@ -58,6 +56,7 @@ async function getQuestions() {
         });
     }
 
+    // Imposto il timer in base alla difficoltà delle domande
     switch (true) {
         case questions[contatore].difficulty == 'easy':
             TIME_LIMIT = 30;
@@ -72,12 +71,13 @@ async function getQuestions() {
             TIME_LIMIT = 5;
     }
 
-    //definisco un'area in cui inserire il clone
+    // Definisco un'area in cui inserire il clone
     let target = document.getElementById("target");
 
-    //inserisco il clone
+    // Inserisco il clone
     target.append(clone);
 
+    // Seleziono l'elemento con l'id "app" e costruisco l'HTML del timer
     document.getElementById("app").innerHTML = `
             <div class="base-timer">
                 <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
@@ -107,8 +107,8 @@ async function getQuestions() {
     //faccio partire il timer
     startTimer();
 
+    // QuerySelector per aggiornare dinamicamente il conteggio delle domande in basso nel quiz
     document.querySelector('#currentQuestion').innerHTML = contatore + 1;
-
     document.querySelector('#totalQuestions').innerHTML = questions.length;
 
     //funzioni relative al timer
@@ -117,7 +117,6 @@ async function getQuestions() {
         risposte.sbagliate++;
         contatore++;
         clearTarget();
-        risposte["risposte date"].push('Risposta non data');
     }
 
     function startTimer() {
@@ -168,14 +167,17 @@ async function getQuestions() {
 
         //controlla se ci sono domande rimanenti
         if (contatore < questions.length) {
+            // Svuota il DIV con ID "target"
             targetTemp.innerHTML = '';
-
             //mostra le domande
             getQuestions();
         }
         else {
+            // Svuota il DIV con ID "target"
             targetTemp.innerHTML = '';
-            clone1 = risultatiTest.content.cloneNode(true);
+
+            // Clona la pagina dei risultati del test
+            let clone1 = risultatiTest.content.cloneNode(true);
             targetTemp.append(clone1);
             let corrette = document.querySelector('.esito');
             let errate = document.querySelector('.esito2');
@@ -191,11 +193,48 @@ async function getQuestions() {
             <h3>Wrong <b>${percentualeSbagliate} %</b></h3>
             <p> ${risposte.sbagliate}/${questions.length} questions </p>
             `;
+
+            console.log(percentualeCorrette < 60);
+
+            // Se la percentuale delle disposte corrette è inferiore a 60
+            if( percentualeCorrette < 60  ){
+
+                // Seleziono il cerchio e lo coloro di rosso
+                let circle = document.querySelector('.window3 #circle');
+                circle.style.borderColor = 'red';
+                circle.style.marginTop = '-440px';
+
+                // Cambio il testo dell'h4
+                let h4 = document.querySelector('.window3 h4');
+                h4.innerHTML = `
+                <h4>We're Sorry!</h4>
+                <p> <span>You didn't passed the exam.</span> </p>
+                `
+                // Coloro di rosso
+                let span = document.querySelector('.window3 h4 span');
+                span.style.color = 'red';
+
+            }else{
+
+                
+                let h4 = document.querySelector('.window3 h4');
+                h4.innerHTML = `
+                <h4>Congratulations! <span> You passed the exam. </span> </h4>
+                <p>We'll send you the certificate in few minutes. Check your email &lpar;including promotions / spam folder&rpar;</p>
+                `
+            }
+
+            document.querySelector('#bottone button').addEventListener('click', function () {
+                target.innerHTML = '';
+                clone1 = feedback.content.cloneNode(true);
+                target.append(clone1);
+                stelle();
+            });
         }
     }
 }
 
-//funzione per mescolare gli elementi di un array
+// Funzione per mescolare gli elementi di un array
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
@@ -204,66 +243,108 @@ function shuffleArray(array) {
     return array;
 }
 
-//clono il contenuto, generando ogni volta un nuovo clone
+// Clono il contenuto, generando ogni volta un nuovo clone
 let clone1 = primaPagina.content.cloneNode(true);
 
-//definisco un'area in cui inserire il clone
+// Definisco un'area in cui inserire il clone
 let target = document.getElementById("target");
 
-//inserisco il clone
+// Inserisco il clone
 target.append(clone1);
 
-//seleziono il bottone per procedere con il quiz dalla schermata
+// Seleziono il bottone per procedere con il quiz dalla schermata
 let proceed = document.querySelector('#botton .bottone1');
 
+// Al click del bottone PROCEED si svuota la pagina corrente
 proceed.addEventListener('click', function () {
     target.innerHTML = '';
-    //eseguo la funzione getQuestions
+    // Eseguo la funzione getQuestions per iniziare il Quiz
     getQuestions();
 })
 
-
-//     //se non ci sono domande mostra il punteggio
-//     targetTemp.innerHTML = `
-//     <p>Giuste: ${risposte.giuste}</p>
-//     <p>Sbagliate: ${risposte.sbagliate}</p>
-//     <p>Le tue risposte:</p>
-//     <ol id="lista">
-//     <ol>
-//     `;
-//     for (let elemento of risposte["risposte date"]) {
-//         document.getElementById('lista').innerHTML += `<li>${elemento}</li>`;
-//     }
-
-
+// Funzione per calcolare la percentuale di risposte corrette
 function calcolaCorrette(giuste, totale) {
     let total = (giuste / totale) * 100;
+    if (total % 2 != 0) {
+        total = total.toFixed(2);
+    }
     return total;
 }
 
+// Funzione per calcolare la percentuale di risposte sbagliate
 function calcolaSbagliate(sbagliate, totale) {
     let total = (sbagliate / totale) * 100;
+    if (total % 2 != 0) {
+        total = total.toFixed(2);
+    }
     return total;
 }
 
-
-
-
-
-//funzione per stampare le 10 stelle per il feedback
+// Funzione per stampare le 10 stelle per il feedback
 function stelle() {
 
-    let stelle = document.querySelector('#stelle');
+    // Seleziono il DIV contenitore delle stelle
+    let stelleDiv = document.querySelector('#stelle');
 
+    // Creo le 10 stelle
     for (let index = 1; index <= 10; index++) {
 
-        stelle.innerHTML += `<label aria-label="${index} stars" class="rating__label" for="rating-${index}"><svg class="rating__icon--star" width="47" height="46" viewBox="0 0 47 46" xmlns="http://www.w3.org/2000/svg">
-                <path d="M22.2044 1.55551C22.6143 0.569963 24.0104 0.569964 24.4203 1.55552L29.9874 14.9402C30.1602 15.3557 30.5509 15.6396 30.9994 15.6756L45.4494 16.834C46.5134 16.9193 46.9448 18.2471 46.1341 18.9415L35.1248 28.3722C34.7831 28.6649 34.6338 29.1242 34.7382 29.5619L38.1018 43.6626C38.3494 44.7009 37.2199 45.5215 36.309 44.9651L23.9379 37.4089C23.5538 37.1743 23.0709 37.1743 22.6868 37.4089L10.3157 44.9651C9.40478 45.5215 8.27528 44.7009 8.52295 43.6626L11.8865 29.5619C11.9909 29.1242 11.8416 28.6649 11.4999 28.3722L0.490575 18.9415C-0.320069 18.2471 0.111362 16.9193 1.17535 16.834L15.6253 15.6756C16.0738 15.6396 16.4645 15.3557 16.6374 14.9402L22.2044 1.55551Z"/>
-                </svg>
-                </label>
-                <input class="rating__input" name="rating" id="rating-${index}" value="${index}" 
-            type="radio">
-            `
+        stelleDiv.innerHTML += `<svg width="47" height="46" viewBox="0 0 47 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M22.2044 1.55551C22.6143 0.569963 24.0104 0.569964 24.4203 1.55552L29.9874 14.9402C30.1602 15.3557 30.5509 15.6396 30.9994 15.6756L45.4494 16.834C46.5134 16.9193 46.9448 18.2471 46.1341 18.9415L35.1248 28.3722C34.7831 28.6649 34.6338 29.1242 34.7382 29.5619L38.1018 43.6626C38.3494 44.7009 37.2199 45.5215 36.309 44.9651L23.9379 37.4089C23.5538 37.1743 23.0709 37.1743 22.6868 37.4089L10.3157 44.9651C9.40478 45.5215 8.27528 44.7009 8.52295 43.6626L11.8865 29.5619C11.9909 29.1242 11.8416 28.6649 11.4999 28.3722L0.490575 18.9415C-0.320069 18.2471 0.111362 16.9193 1.17535 16.834L15.6253 15.6756C16.0738 15.6396 16.4645 15.3557 16.6374 14.9402L22.2044 1.55551Z"/>
+        </svg>`
     }
+
+    // AGGIUNGERE HOVER SU TUTTE LE STELLE
+    function aggiungiHover() {
+        let stelle = document.querySelectorAll('#stelle svg');
+        stelle.forEach(function (questeStelle) {
+            questeStelle.addEventListener("mouseover", illumina);
+            questeStelle.addEventListener("mouseout", togliIllumina);
+        });
+    }
+
+    // ADD HIGHLIGHT CLASS ON HOVER
+    function illumina() {
+        let seleziona = document.querySelectorAll("#stelle svg");
+        let starIndex = Array.from(seleziona).indexOf(this);
+        for (let i = 0; i <= starIndex; i++) {
+            seleziona[i].classList.add("starActive");
+        }
+    }
+
+    // La funzione "illumina" = il variabile "starIndex"  (che a sua volta è stata selezionata da "seleziona"), attiva il metodo "Array.from" che sempre con il ciclo divide le stelle
+    // una volta alla volta e con "indexOf(this)" cioè seleziona "QUESTO-THIS"  cioe l'elemento su cui si trova il mouse in quel preciso momento.
+    // poi per ogni stella aggiunge la classe starActive definito in css prima.
+
+    // TOGLIERE LA FUNZIONE ILLUMINA 
+    function togliIllumina() {
+        let stars = document.querySelectorAll("#stelle svg");
+        stars.forEach(function (star) {
+            star.classList.remove("starActive");
+        });
+    }
+
+    // RIEMPIRE LE STELLE FINCHE QUELLA DESIDERATA NON VIENE CLICCATA
+    function fillStars(target) {
+        let activeStarClass = "starActive";
+        let inactiveStarClass = "starInactive";
+        let stars = document.querySelectorAll("#stelle svg path");
+        let starIndex = Array.from(stars).indexOf(target);
+        for (let i = 0; i <= starIndex; i++) {
+            stars[i].classList.add(activeStarClass);
+            stars[i].classList.remove(inactiveStarClass);
+        }
+        for (let i = starIndex + 1; i < stars.length; i++) {
+            stars[i].classList.remove(activeStarClass);
+            stars[i].classList.add(inactiveStarClass);
+        }
+    }
+
+    // ADD CLICK EVENT LISTENER TO STARS CONTAINER
+    stelleDiv.addEventListener("click", function (event) {
+        fillStars(event.target);
+    });
+
+    aggiungiHover();
 
 }
